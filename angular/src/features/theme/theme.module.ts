7f7bf99dda5b3bddbff4186ed5a2ae4a5509af77
Laurie MORIN <laurie20010701@gmail.com>
2023-03-31 11:42:19 +0200

    accueil

---
+++ angular/src/features/theme/theme.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { ThemeRoutingModule } from './theme-routing.module';
+
+import { ThemeComponent } from './components/theme.component';
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatCardModule} from "@angular/material/card";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatIconModule} from "@angular/material/icon";
+import {CommonModule} from "@angular/common";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+
+@NgModule({
+  declarations: [
+    ThemeComponent,
+  ],
+  imports: [
+    ThemeRoutingModule,
+    MatTabsModule,
+    MatCardModule,
+    MatChipsModule,
+    MatIconModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatProgressSpinnerModule
+  ]
+})
+export class ThemeModule { }
