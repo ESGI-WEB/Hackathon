90b0b42582c541c20588aaf94d1b18e889530906
Nicolas W <nwadoux@myges.fr>
2023-03-31 14:24:04 +0200

    change token implementation

--- angular/src/app/services/content.service.ts
+++ angular/src/app/services/content.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {map, Observable} from "rxjs";
 import {Content, PostContent} from "../models/content";
+import {AuthService} from "./auth.service";
 
 export interface SearchContent {
   name: string;
@@ -14,12 +15,12 @@ export interface SearchContent {
 })
 export class ContentService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   postContent(content: PostContent): Observable<Content> {
     return this.http.post<Content>('https://localhost/contents', content, {
       headers: {
-        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2ODAyNTk3NTAsImV4cCI6MTY4MDM0NjE1MCwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImVtYWlsIjoidXNlckB1c2VyLmZyIn0.X1Bn4AejpXR2H2H1h0jvQhZgmLPmO30LYL1oiu6q8e57EHuaI97tkZFy2DvwyVALEUzIgGuwlGjaL95m3FoZ6LpAp2i6DyFSFpmyXiBK7Eydbi-kRIddwqfeCHzjsTj9AomRDDC-DBi7yIN9TyStjcyg5cTdZj7LWr2J1SUPHBQC7KpKVTJUEZwFndoyAS1SWaDLRRf0dVDlSfBOkmTpWmbsplzsrC7Z4z4e8MhW8lN403K5XMdet6FLZejmjVwYgnEe8QvbFf3tcCNNsFeErKg9Avw0OsWKCfwDh-SODfZDks-5kOChBfdePop45TA59JJYpUJvOgV4fE8BvGDNcw'
+        'Authorization': 'Bearer ' + this.authService.getToken()
       }
     });
   }
